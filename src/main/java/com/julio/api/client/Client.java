ccff9924916aad5ed11cdc20d8f8eebf49316d64
Julio Rosario <rosariojulio40@gmail.com>
2021-02-15 15:19:14 -0500

    Configure soap client to work with soap 1.2

--- src/main/java/com/julio/api/client/Client.java
+++ src/main/java/com/julio/api/client/Client.java
@@ -1,13 +1,25 @@
 package com.julio.api.client;
 
-import com.julio.api.client.model.Exam;
+
 import com.julio.api.client.model.InputRequest;
 import com.julio.api.client.model.OutputRequest;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.oxm.jaxb.Jaxb2Marshaller;
-import org.springframework.stereotype.Service;
+
+import org.springframework.ws.InvalidXmlException;
+import org.springframework.ws.WebServiceMessage;
+import org.springframework.ws.WebServiceMessageFactory;
 import org.springframework.ws.client.core.WebServiceTemplate;
 import org.springframework.ws.client.core.support.WebServiceGatewaySupport;
+import org.springframework.ws.soap.SoapVersion;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
+
+import javax.xml.soap.MessageFactory;
+import javax.xml.soap.SOAPConstants;
+import javax.xml.soap.SOAPException;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import static javax.xml.soap.SOAPConstants.SOAP_1_2_PROTOCOL;
 
 
 public class Client extends WebServiceGatewaySupport {
@@ -16,13 +28,24 @@ public class Client extends WebServiceGatewaySupport {
 
     public OutputRequest inputRequest(InputRequest request) {
 
-        OutputRequest outputRequest = new OutputRequest();
-        Exam exam = request.getExam();
+        WebServiceTemplate template = getWebServiceTemplate();
 
-        double total = exam.getTest1() + exam.getTest2() + exam.getTest3();
+        MessageFactory msgFactory = null;
+        try {
+            msgFactory = MessageFactory.newInstance(SOAPConstants.SOAP_1_2_PROTOCOL);
+        } catch (SOAPException e) {
+            e.printStackTrace();
+        }
 
-        outputRequest.setGrade(total/3);
+        SaajSoapMessageFactory newSoapMessageFactory = new SaajSoapMessageFactory(msgFactory);
+        template.setMessageFactory(newSoapMessageFactory);
 
-        return outputRequest;
+
+            OutputRequest response = (OutputRequest) template
+                    .marshalSendAndReceive("http://localhost:8080/ws", request);
+
+//            SOAPException e
+
+        return response;
     }
 }
--- src/main/java/com/julio/api/client/SoapConfig.java
+++ src/main/java/com/julio/api/client/SoapConfig.java
@@ -3,9 +3,20 @@ package com.julio.api.client;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.oxm.jaxb.Jaxb2Marshaller;
+import org.springframework.ws.WebServiceMessageFactory;
+import org.springframework.ws.soap.SoapVersion;
+import org.springframework.ws.soap.saaj.SaajSoapMessageFactory;
 
 @Configuration
 public class SoapConfig {
+
+
+    @Bean
+    public WebServiceMessageFactory messageFactory(){
+        SaajSoapMessageFactory messageFactory = new SaajSoapMessageFactory();
+        messageFactory.setSoapVersion(SoapVersion.SOAP_12);
+        return  messageFactory;
+    }
 
     @Bean
     public Jaxb2Marshaller marshaller() {
